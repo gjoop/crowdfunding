c3e95fef72ce931c777ae457d4ae7f6b1bc8b447
joop_ <joop_@192.168.1.4>
2016-08-10 01:28:07 +1100

    changement favicon

--- greenlight/index.php
+++ greenlight/index.php
@@ -7,11 +7,12 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Greenlight</title>
+        <link rel="icon" type="image/png" href="media/favicon.png" />
     </head>
     <body>
         <?php
-        // put your code here
+            // put your code here
         ?>
     </body>
 </html>
