05aa69582588a37e0c8e5b4d7f92f4c4640ec114
joop_ <joop_@192.168.1.4>
2016-08-25 09:44:41 +1100

    indentation faites. rajout d'un lorem ipsum. création d'un css basique

--- greenlight/page_d'accueil.php
+++ greenlight/page_d'accueil.php
@@ -5,18 +5,54 @@
 	<meta name="keywords" content="projet, crowdfunding, calédonie, université" />
 	<meta name="description" content="Greenlight" />
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<!--	<link rel="stylesheet" href="style_test.css" /> -->
+ 	<link rel="stylesheet" href="style.css" />
 	<link rel="icon" type="image/png" href="media/favicon.png" />
 </head>
-
-<body >
-
-<header>
-	<img src="media/favicon.png" alt="Icone feu" />
-	<h1>Greelight</h1>
-
-</header>
-
-</body>
-
+    <body>
+        <header>
+            <h1>
+                <img src="media/favicon.png" alt="Icone feu" />
+                Greenlight
+            </h1>
+        </header>
+        
+        <section>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed 
+            non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+            nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas 
+            ligula massa, varius a, semper congue, euismod non, mi. Proin 
+            porttitor, orci nec nonummy molestie, enim est eleifend mi, non 
+            fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, 
+            scelerisque vitae, consequat in, pretium a, enim. Pellentesque 
+            congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum
+            bibendum augue. Praesent egestas leo in pede. Praesent blandit odio 
+            eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum 
+            ante ipsum primis in faucibus orci luctus et ultrices posuere 
+            cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque 
+            fermentum. Maecenas adipiscing ante non diam sodales hendrerit.Ut 
+            velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut 
+            orci vel massa suscipit pulvinar. Nulla sollicitudin. Fusce varius, 
+            ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus 
+            sapien eros vitae ligula. Pellentesque rhoncus nunc et augue. 
+            Integer id felis. Curabitur aliquet pellentesque diam. Integer quis 
+            metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, 
+            consectetuer adipiscing elit. Morbi vel erat non mauris convallis 
+            vehicula. Nulla et sapien. Integer tortor tellus, aliquam faucibus, 
+            convallis id, congue eu, quam. Mauris ullamcorper felis vitae erat. 
+            Proin feugiat, augue non elementum posuere, metus purus iaculis 
+            lectus, et tristique ligula justo vitae magna.Aliquam convallis 
+            sollicitudin purus. Praesent aliquam, enim at fermentum mollis, 
+            ligula massa adipiscing nisl, ac euismod nibh nisl eu lectus. Fusce 
+            vulputate sem at sapien. Vivamus leo. Aliquam euismod libero eu 
+            enim. Nulla nec felis sed leo placerat imperdiet. Aenean suscipit 
+            nulla in justo. Suspendisse cursus rutrum augue. Nulla tincidunt 
+            tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus, felis 
+            magna fermentum augue, et ultricies lacus lorem varius purus. 
+            Curabitur eu amet.
+        </section>
+        
+        <footer>
+            
+        </footer>
+    </body>
 </html>
