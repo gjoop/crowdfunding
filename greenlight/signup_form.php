abdaced2413c4229bcc89db9246eacee1ff93e41
joop_ <joop_@192.168.1.4>
2016-09-23 05:35:29 +1100

    formulaire complet

--- greenlight/signup_form.php
+++ greenlight/signup_form.php
@@ -1,68 +1,56 @@
 <?php include("./include/head.php"); ?>
 
 <body>
-    <?php include("./include/header.php"); ?>
-    
-    <form method="post" action="hello.php"> 
-        
-        	<label for="login"></label>     
-        	<input type="text" name="login" id="login" placeholder="Login" /> 
-                
-                </br> </br>
-                
-        	<label for="password"></label> 
-                <input type="password" name="password" id="password" placeholder="PassWord"/>
-                
-                </br> </br>
-                
-                <label for="email"><label>
-                <input type="email" name="email" id="email" placeholder="Email"/>
-                
-                </br> </br>
-                
-                <label for="adresse"><label>
-                <input type="adresse" name="adresse" id="adresse" placeholder="Adresse"/>
-        
-                </br> </br>
-                
-                <label for="Téléphone"><label>
-                <input type="Téléphone" name="Téléphone" id="Téléphone" placeholder="Téléphone"/>
-                
-                </br> </br>
-                
-                <label for="Site Web"><label>
-                <input type="Site Web" name="Site Web" id="Site Web" placeholder="Site Web"/>
-                
-                </br> </br>
-                
-                <label for="Nom Association"><label>
-                <input type="Nom Association" name="Nom Association" id="Nom Association" placeholder="Nom Association"/>
-                
-                </br> </br>
-                
-                <label for="BankAccount"><label>
-                <input type="BankAccount" name="BankAccount" id="BankAccount" placeholder="BankAccount"/>
-                
-                </br> </br>
-        	
-                <input type="submit" value="Inscription"> 
-        	
-        	
-    	</form>
-    
-    <footer>
-        
-    </footer>
 
-</body>
+<?php include("./include/header_alt.php");
 
-</html>
+if ($_POST['type']=='user') {
+?>
+    
+    <form method="post" action="signup.php">
+        <input type="hidden" name="type" id="type" value="user" />
+        <input required type="text" name="login" id="login" placeholder="Identifiant" />
+        <input required type="password" name="password" id="password" placeholder="Mot de passe"/>
+        <input type="text" name="nom" id="nom" placeholder="Nom"/>
+        <input type="text" name="prenom" id="prenom" placeholder="Prénom"/>
+        <input required type="email" name="email" id="email" placeholder="e-mail"/>
+        <input type="text" name="address" id="address" placeholder="Adresse"/>
+        <input type="tel" name="phone" id="phone" placeholder="Numéro de téléphone"/>
+        <input type="radio" name="sex" value=male id="male"> <label for="male">Homme</label>
+        <input type="radio" name="sex" value=female id="female"> <label for="female">Femme</label>
+        <input type="radio" name="sex" value=unassigned id="unassigned"> <label for="unassigned">Autre</label>
+        <input type="date" name="birth" id="birth" placeholder="Date de naissance"/>
+        <input type="number" name="bankAccount" id="bankAccount" placeholder="Numéro de compte"/>
+        <input type="submit" value="S'inscrire">
+        <input type="reset" value="Effacer le formulaire">
+    </form>
+    
+<?php
+
+}
+
+else {
+
+?>
+    <form method="post" action="signup.php">
+        <input type="hidden" name="type" id="type" value="association" />
+        <input required type="text" name="login" id="login" placeholder="Identifiant" />
+        <input required type="password" name="password" id="password" placeholder="Mot de passe"/>
+        <input required type="text" name="nomAssociation" id="nomAssociation" placeholder="Nom d'association"/>
+        <input required type="email" name="email" id="email" placeholder="abc@xyz.org"/>
+        <input required type="text" name="address" id="address" placeholder="Adresse"/>
+        <input required type="tel" name="phone" id="phone" placeholder="Numéro de téléphone"/>
+        <input type="url" name="website" id="website" placeholder="Site web"/>
+        <input required type="number" name="bankAccount" id="bankAccount" placeholder="Numéro de compte"/>
+        <input type="submit" value="S'inscrire">
+        <input type="reset" value="Effacer le formulaire">
+    </form>
 
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+}
+include("./include/footer.php");
 
+?>
+
+</body>
