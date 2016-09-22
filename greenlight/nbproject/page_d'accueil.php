e955dd467aa8341ab99a6e0828e693ffba763106
joop_ <joop_@192.168.1.4>
2016-09-22 18:48:47 +1100

    restructuration du code source

---
+++ greenlight/nbproject/page_d'accueil.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title> GREENLIGHT </title>
+	<meta name="keywords" content="projet, crowdfunding, calédonie, université" />
+	<meta name="description" content="Greenlight" />
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<!--	<link rel="stylesheet" href="style_test.css" /> -->
+	<link rel="icon" type="image/png" href="media/favicon.png" />
+</head>
+
+<body >
+
+<header>
+    <h1>
+        <img src="media/favicon.png" alt="Icone feu" />
+	Greelight
+    </h1>
+    
+    <form method="post" action="index.php"> 
+        
+        <label for="login"> Votre identifiant </label> :      
+        <input type="text" name="login" id="login" placeholder="defaut" /> 
+        
+        <br /> 
+        
+        <label for="password"> Votre mot de passe </label> :     
+        <input type="password" name="password" id="password" /> 
+        
+        <input type="submit" value="Envoyer"> 
+    </form>  
+
+</header>
+
+</body>
+
+</html>
