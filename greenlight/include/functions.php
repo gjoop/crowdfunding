24c5d564ff43e62ab1a7f144450cf2c25bd7b979
joop_ <joop_@192.168.1.4>
2016-09-23 05:37:10 +1100

    fonctions d'écriture ajouté

--- greenlight/include/functions.php
+++ greenlight/include/functions.php
@@ -2,4 +2,8 @@
     $user_info = $db->prepare('SELECT * FROM user WHERE login = ? AND password = ?');
     $association_info = $db->prepare('SELECT * FROM association WHERE login = ? AND password = ?');
 
+    $new_user = $db->prepare('INSERT INTO user(login, password, nom, prenom, email, address, phone, sex, birth, bankAccount) VALUES(:login, :password, :nom, :prenom, :email, :address, :phone, :sex, :birth, :bankAccount)');
+    $new_association = $db->prepare('INSERT INTO user(login, password, nomAssociation, email, address, phone, website, bankAccount) VALUES(:login, :password, :nomAssociation, :email, :address, :phone, :website, :bankAccount)');
+
+    
 ?>
