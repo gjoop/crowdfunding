9ed6e750409296bee9d7033ef16803de7ca7ef32
joop_ <joop_@192.168.1.4>
2016-09-22 18:46:29 +1100

    formulaire d'inscription (vide)

---
+++ greenlight/signup_form.php
@@ -0,0 +1,68 @@
+<?php include("./include/head.php"); ?>
+
+<body>
+    <?php include("./include/header.php"); ?>
+    
+    <form method="post" action="hello.php"> 
+        
+        	<label for="login"></label>     
+        	<input type="text" name="login" id="login" placeholder="Login" /> 
+                
+                </br> </br>
+                
+        	<label for="password"></label> 
+                <input type="password" name="password" id="password" placeholder="PassWord"/>
+                
+                </br> </br>
+                
+                <label for="email"><label>
+                <input type="email" name="email" id="email" placeholder="Email"/>
+                
+                </br> </br>
+                
+                <label for="adresse"><label>
+                <input type="adresse" name="adresse" id="adresse" placeholder="Adresse"/>
+        
+                </br> </br>
+                
+                <label for="Téléphone"><label>
+                <input type="Téléphone" name="Téléphone" id="Téléphone" placeholder="Téléphone"/>
+                
+                </br> </br>
+                
+                <label for="Site Web"><label>
+                <input type="Site Web" name="Site Web" id="Site Web" placeholder="Site Web"/>
+                
+                </br> </br>
+                
+                <label for="Nom Association"><label>
+                <input type="Nom Association" name="Nom Association" id="Nom Association" placeholder="Nom Association"/>
+                
+                </br> </br>
+                
+                <label for="BankAccount"><label>
+                <input type="BankAccount" name="BankAccount" id="BankAccount" placeholder="BankAccount"/>
+                
+                </br> </br>
+        	
+                <input type="submit" value="Inscription"> 
+        	
+        	
+    	</form>
+    
+    <footer>
+        
+    </footer>
+
+</body>
+
+</html>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
